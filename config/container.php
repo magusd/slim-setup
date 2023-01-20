1fcd875945ff7472301df2e6d4c09a946e8be1d1
Davi Martins <davi.senra98@gmail.com>
2023-01-19 23:54:03 -0300

    Added Slim-http dependency, refactored repository classes

--- config/container.php
+++ config/container.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Repository\UserRepository;
 use DI\Container;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
@@ -11,13 +10,13 @@ use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 return function (Container $container) {
-    $container->set('settings', require __DIR__ . '/settings.php');
+    $container->set('settings', require __DIR__.'/settings.php');
 
     $container->set(EntityManager::class, function (Container $c): EntityManagerInterface {
         /** @var array $settings */
         $settings = $c->get('settings');
 
-        $cache = $settings['doctrine']['dev_mode'] ? 
+        $cache = $settings['doctrine']['dev_mode'] ?
             new ArrayAdapter() :
             new FilesystemAdapter(directory: $settings['doctrine']['cache_dir']);
 
@@ -31,10 +30,6 @@ return function (Container $container) {
         return EntityManager::create($settings['doctrine']['connection'], $config);
     });
 
-    $container->set(UserRepository::class, function (Container $c) {
-        return new UserRepository($c->get(EntityManager::class));
-    });
-
     $container->set(App::class, function (Container $c): App {
         $app = AppFactory::createFromContainer($c);
 
@@ -42,10 +37,10 @@ return function (Container $container) {
         $app->addRoutingMiddleware();
         $app->addErrorMiddleware(true, false, false);
 
-        $routes = require __DIR__ . '/../routes/web.php';
+        $routes = require __DIR__.'/../routes/web.php';
         $routes($app);
 
-        $api = require __DIR__ . '/../routes/api.php';
+        $api = require __DIR__.'/../routes/api.php';
         $api($app);
 
         return $app;
