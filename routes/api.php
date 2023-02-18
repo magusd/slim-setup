f9cb12b8c1c20380baed9c6d44615541c306de54
magusd <vitor.lobs@gmail.com>
2023-02-17 21:32:54 -0300

    basic home route

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,11 @@ use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
 
 return function (App $app) {
+    $app->get('/', function($request, $response){
+        $response->getBody()->write('Yo yo, checkout the <a href="https://github.com/davisenra/slim-setup">docs</a> ');
+        return $response;
+    });
+
     $app->group('/api', function (RouteCollectorProxy $group) {
         $group->get('/users', [UserController::class, 'index']);
         $group->post('/users', [UserController::class, 'store']);
